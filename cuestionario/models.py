c30b8c5c929e3a5173426c23c0fd75123d461612
GustavoValdesZ <gustavovaldes.z@gmail.com>
2026-03-12 16:43:41 -0300

    se añade atributo es_coordinador a la tabla trabajador en models y script creador de tablas

--- cuestionario/models.py
+++ cuestionario/models.py
@@ -343,6 +343,7 @@ class Trabajador(models.Model):
     apellido_materno = models.CharField(max_length=40)
     email = models.CharField(max_length=80)
     genero = models.CharField(max_length=10)
+    es_coordinador = models.BooleanField(default=False, db_column='ES_COORDINADOR')
     
     empresa = models.ForeignKey(
         'Empresa', 
